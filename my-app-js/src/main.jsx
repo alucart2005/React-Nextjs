0bc186acfcde92e63f23bafaa392746f7eedade8
Napoleon Anaya Romero <113813382+alucart2005@users.noreply.github.com>
2025-11-12 10:02:45 -0500

    Versión 0.1

--- my-app-js/src/main.jsx
+++ my-app-js/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+          <Route path='/home' element={<p>Hola home - desde el home</p>}/>
+          <Route path='/by' element={<p>Adios by  - desde el by</p>}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
